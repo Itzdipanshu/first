2d148bacb28079e1f6ef1212640270bce9ffed65
Itzdipanshu <dipanshug676@gmail.com>
2025-01-22 18:12:01 +0530

    this is the message

--- first.java
+++ first.java
@@ -1,6 +1,7 @@
 class first{
     public static void main(String[] args) {
-        System.out.println("hello dosto");
-        System.out.Println("this change is made for learning github");
+        System.out.print("hello dosto");
+        System.out.println("this change is made for learning github");
+        System.out.print("this is the third change that I made today ");
     }
 }
