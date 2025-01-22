92126b1c293fd638b0df6849e05fe46c09eded9b
Dipanshu Gupta <121938114+Itzdipanshu@users.noreply.github.com>
2025-01-22 19:02:54 +0530

    Update first.java
    added a loop

--- first.java
+++ first.java
@@ -3,5 +3,7 @@ class first{
         System.out.print("hello dosto");
         System.out.println("this change is made for learning github");
         System.out.print("this is the third change that I made today ");
+        for(int i = 0 :i < 5 ;i++ ){
+            System.out.print("print the i"+[i]);
     }
 }
