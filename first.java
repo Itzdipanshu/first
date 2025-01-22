e91220d58d95a2a7c87d281611cab86fbba5036c
Dipanshu Gupta <121938114+Itzdipanshu@users.noreply.github.com>
2025-01-22 14:58:47 +0530

    Update first.java
    changes made for learning purpose

--- first.java
+++ first.java
@@ -1,5 +1,6 @@
 class first{
     public static void main(String[] args) {
         System.out.println("hello dosto");
+        System.out.Println("this change is made for learning github");
     }
 }
